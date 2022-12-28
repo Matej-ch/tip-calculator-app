be73b618599dd054f16f9d69d20bffcf5d012ee5
Matej <matejchalachan@gmail.com>
2022-12-28 22:11:12 +0100

    added function to tip selector

--- src/TipSelector.tsx
+++ src/TipSelector.tsx
@@ -1,17 +1,37 @@
 import './TipSelector.scss'
+import React, {useState} from "react";
 
 function TipSelector({options}: { options: number[] }) {
 
+    const [selectedTip, setSelectedTip] = useState<number>(-1);
+    const [isCustomTip, setIsCustomTip] = useState<boolean>(false);
+    const [customTip, setCustomTip] = useState('25');
 
+    const handleBtnClick = (index: number): void => {
+        setSelectedTip(index);
+        setIsCustomTip(false);
+    }
+
+    const handleCustomTip = (): void => {
+        setSelectedTip(-1);
+        setIsCustomTip(true)
+    }
+
+    const handleCustomTipChange = (e: React.ChangeEvent<HTMLInputElement>): void => setCustomTip(e.target.value);
 
     return (<div className={'tip-selector'}>
         <label htmlFor="">Select tip %</label>
         <div className={'options'}>
             {options.map((tip, index) => {
-                return <button className={'tip-box'} key={index}>{tip}%</button>
+                return <button onClick={() => handleBtnClick(index)} className={selectedTip === index ? 'selected' : ''}
+                               key={index}>
+                    {tip}%
+                </button>
             })}
 
-            <button className={'tip-box'}>Custom</button>
+            {isCustomTip ?
+                <input className={'custom-tip'} type="text" value={customTip} onChange={handleCustomTipChange}/> :
+                <button onClick={() => handleCustomTip()} className={'custom-tip'}>Custom</button>}
         </div>
 
     </div>);
